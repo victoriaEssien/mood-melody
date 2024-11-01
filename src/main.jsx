c0e7a821b4b09849fe9fbd9863f6f4a097a34879
victoriaEssien <victoriaessien857@gmail.com>
2024-11-01 06:09:26 +0100

    initial project structure and tailwind installation

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,8 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
+import { createRoot } from 'react-dom/client';
 import App from './App.jsx'
+import './index.css'
 
-createRoot(document.getElementById('root')).render(
-  <StrictMode>
-    <App />
-  </StrictMode>,
-)
+const domNode = document.getElementById('root');
+const root = createRoot(domNode);
+
+root.render(<App />);
